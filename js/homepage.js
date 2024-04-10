6e6f6bbafa727d7d8b5d9f2615fcf59fbd522a12
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2024-04-10 22:45:16 +0200

    Homepage done + added new files
    - New Media Folder containing pngs
    - New CSS folder containing css files
    - Updated index.html with header links and new scripts in body
    - Added 2 files to the JS folder, index.js for general functions and variables, homepage.js for rendering the home page

---
+++ js/homepage.js
@@ -0,0 +1,46 @@
+body.innerHTML = `
+    <container id="homepage">
+        <div class="browserTopInput">browser top input section</div>
+        <div id="home1">
+            <h1>Hej!</h1>
+            <p>Nalle Puh har tappat bort <br> sin favorithonung! Vad <br> ska han göra nu? Kan du <br> hjälpa honom?</p>
+            <div id="homeImg1"></div>
+        </div>
+        <div id="home2">
+            <div id="home2Box">
+                <h2>Om Nalle Puh och hans vänner</h2>
+                <div id="scrollContainer">
+                    <div class="scrollImg">
+                        <div id="nallePuhHomeImg"></div>
+                        <p>Nalle Puh</p>
+                    </div>
+                    <div class="scrollImg">
+                        <div id="iorHomeImg"></div>
+                        <p>I-or</p>
+                    </div>
+                    <div class="scrollImg">
+                        <div id="tigerHomeImg"></div>
+                        <p>Tiger</p>
+                    </div>
+                    <div class="scrollImg">
+                        <div id="nasseHomeImg"></div>
+                        <p>Nasse</p>
+                    </div>
+                    <div class="scrollImg">
+                        <div id="kaninHomeImg"></div>
+                        <p>Kanin</p>
+                    </div>
+                    <div class="scrollImg">
+                        <div id="ruHomeImg"></div>
+                        <p>Ru</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="home3">
+            <p>Hjälp Nalle Puh och hand vänner genom att genomföra skattjakten</p>
+            <button id="start">Starta</button>
+        </div>
+    </container>
+`;
