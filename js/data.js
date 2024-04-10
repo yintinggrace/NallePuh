8c61eeeb73290b25e89fa5edb4fd2290c044e63e
TheaGenrup <theagenrup@gmail.com>
2024-04-10 16:10:51 +0200

    Array
    Så här tänker jag att den kan se ut

---
+++ js/data.js
@@ -0,0 +1,38 @@
+[{
+    id: 0,
+    questDescription: "Nalle Puh, björnen med stort hjärta och ännu större aptit för honung, har tappat bort sin älskade honungsburk. Paniken spred sig snabbt bland hans vänner när de insåg att deras kära vän var i nöd. Hans vänner befinner sig utspridda i Folkets Park och letar efter honungen. Hjälp hans vänner och Nalle Puh att hitta hans honung genom att fråga dem. Han tar emot all hjälp han kan få och följer gärna med dig då han tror att du kan hjälpa honom extra mycket!",
+    alternativeType: "image",
+    alternatives: [
+        {
+            id: 0,
+            imgSrc: "sökväg"
+        }
+    ],
+    correctAlternative: "något id",
+    character: {
+        name: "I-or",
+        description: ""
+    },
+    map: "??"
+
+}, {
+    id: 1,
+    questDescription: "",
+    alternativeType: "text",
+    alternatives: [
+        {
+            id: 0,
+            text: ""
+        }
+    ],
+    correctAlternative: "något id",
+    character: {
+        name: "Ru",
+        description: ""
+    },
+    tip: "Tack så mycket för tipset om glassen, den verkar jättegod! Jag har däremot inte sett honungsburken men såg Ru borta vid terrariet innan. Jag vill helst inte vistas bland djuren, men har hört att djuren där är mycket förtjusta i honung. Kanske de har tagit och smakat lite på Nalle Puhs honung?",
+    map: "??"
+
+}]
+
+// jag tänker att tipset som gäller Ru får ligga under Ru och inte I-or. Alltså den det gäller, inte den som ger tipset.
