963aa3d057e1053f178498fc575c65118659efc4
Grace <yintingchancytg@gmail.com>
2024-04-11 14:42:26 +0200

    Created icon groups

--- js/station.js
+++ js/station.js
@@ -5,6 +5,20 @@ function displayContent(index) {
 
     document.body.innerHTML = `
       <main class="station-main">
+        <div class="station-buttons">
+          ${createButtonGroup('fas fa-question', 'Tips')}
+          ${createButtonGroup('fas fa-map-marker-alt', 'Karta')}
+          ${createButtonGroup('fas fa-paw', `Om ${currentContent.character.name}`)}
+        </div>
       </main>
     `;
+}
+function createButtonGroup(iconClass, text) {
+  return `
+    <div class="button-group">
+      <div class="icon-container">
+        <i class="${iconClass}"></i>
+      </div>${text}
+    </div>
+  `;
 }
