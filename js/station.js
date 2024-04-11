030f717e3695b4a948ac8de6287ca05fd35ed5be
Grace <yintingchancytg@gmail.com>
2024-04-11 14:37:57 +0200

    Marked up station.js and station.css

---
+++ js/station.js
@@ -0,0 +1,10 @@
+let currentIndex = 0;
+
+function displayContent(index) {
+    const currentContent = data[index];
+
+    document.body.innerHTML = `
+      <main class="station-main">
+      </main>
+    `;
+}
