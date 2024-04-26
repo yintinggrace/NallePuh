8d274895491c91d1f426269394df7a7995dfb49d
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2024-04-26 21:58:20 +0200

    Diploma Name input fixed
    Now the users names are printed on the diploma
    + some small css changes, still working on the design and layout of the diploma

--- js/diploma.js
+++ js/diploma.js
@@ -1,4 +1,22 @@
 function renderDownloadDiplomaPage() {
+    let childrenNames = localStorage.getItem("childrenNames");
+
+    let namesArray = JSON.parse(childrenNames);
+
+    let formattedNames = '';
+    if (namesArray.length === 1) {
+        formattedNames = capitalizeFirstLetter(namesArray[0].trim());
+    } else {
+        formattedNames = namesArray.map(name => capitalizeFirstLetter(name.trim())).join(', ');
+        let lastIndex = formattedNames.lastIndexOf(',');
+        formattedNames = formattedNames.substring(0, lastIndex) + ' och' + formattedNames.substring(lastIndex + 1);
+    }
+
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+
     body.innerHTML = `
     <container id="diplomaPage">
     <div id="coverUp">
@@ -7,9 +25,8 @@ function renderDownloadDiplomaPage() {
     <div id="diploma" class="hidden">
         <!-- Your diploma template with the user's name -->
         <h1>Congratulations!</h1>
-        <p>This diploma is awarded to <span id="username">[Name]</span> for their outstanding achievements.</p>
+        <p>This diploma is awarded to <span id="username">${formattedNames}</span> for their outstanding achievements.</p>
     </div>
     </container>
     `;
 }
-
