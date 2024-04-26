8a29c98a7245ba9dac5a5ffab1c677c6060baaae
KajsaWall <112626806+KajsaWall@users.noreply.github.com>
2024-04-26 16:38:04 +0200

    Working on the Diploma download
    Now it works to download the Diploma in a A4 size. Still working on the design and layout aspects.

--- js/diploma.js
+++ js/diploma.js
@@ -1,12 +1,14 @@
 function renderDownloadDiplomaPage() {
     body.innerHTML = `
     <container id="diplomaPage">
-    <div id="diploma">
+    <div id="coverUp">
+        <button onclick="generateImage()">Download Diploma</button>
+    </div>
+    <div id="diploma" class="hidden">
         <!-- Your diploma template with the user's name -->
         <h1>Congratulations!</h1>
         <p>This diploma is awarded to <span id="username">[Name]</span> for their outstanding achievements.</p>
     </div>
-    <button onclick="generateImage()">Download Diploma</button>
     </container>
     `;
 }
